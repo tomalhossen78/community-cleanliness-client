2b4185cc5dacf49e0b61c84e8bff0bf89521af2c
unknown <tomalhossen78@gmail.com>
2025-11-10 08:20:06 +0600

    profile page added

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { use } from "react";
+import { AuthContext } from "../Context/AuthContext";
+import Container from "../Componets/Container";
 
 const Profile = () => {
+  const { user } = use(AuthContext);
+  const { displayName, email, photoURL } = user;
+  console.log(user);
   return (
-    <div>
-      <h1>This is my profile page</h1>
-    </div>
+    <Container
+      className={"flex flex-col items-center justify-center my-6 space-y-4"}
+    >
+      <h1 className="text-3xl font-semibold">My profile</h1>
+      <img src={photoURL} className="rounded-full" alt="" />
+      <div className="text-center">
+        <h2>Name : {displayName}</h2>
+        <h2>Email : {email}</h2>
+      </div>
+    </Container>
   );
 };
 
